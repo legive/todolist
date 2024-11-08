b92086660e18f0868ce9365f25c128e5a3a30f72
legive <leylagisela@gmail.com>
2024-11-08 08:40:42 -0600

    localStorage funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,18 @@
-import { useState } from "react";
+
 import "./App.css";
-import { tasks } from "./data/db";
 import TaskItem from "./components/TaskItem/TaskItem";
 import TaskForm from "./components/TaskForm/TaskForm";
+import { useTasklist } from "./hooks/useTasklist";
 function App() {
-  const [data, setData] = useState(tasks);
+  const {
+    handledAdd,
+    handleUpdateCheck,
+    handleUpdateTask,
+    handleDelete,
+    completeTasks,
+    notCompleteTasks,
+  } = useTasklist();
 
-  const handledAdd = (task) => {
-    setData([...data, task]);
-  };
-
-  const handleUpdateCheck = (id) => {
-    const index = data.findIndex((task) => task.id === id);
-    const updatedData = [...data];
-    updatedData[index].isComplete = !updatedData[index].isComplete;
-    setData(updatedData);
-    console.log("entro a check",data);
-
-  };
-
-    const handleUpdateTask = (id,name) => {
-      const index = data.findIndex((task) => task.id === id);
-      const updatedData = [...data];
-      updatedData[index].name = name;
-      setData(updatedData);
-      console.log("entro a task")
-    };
-
-  const handleDelete = (id) => {
-    setData(data.filter((task) => task.id != id));
-    console.log("entro a Delete",data);
-  };
-  const completeTasks = data.filter((task) => task.isComplete === true);
-  const notCompleteTasks = data.filter((task) => task.isComplete === false);
   return (
     <>
       <h1>TaskList</h1>
--- src/hooks/useTasklist.js
+++ src/hooks/useTasklist.js
@@ -1,37 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { tasks } from "../data/db";
 export const useTasklist = () => {
-    const [data, setData] = useState(tasks);
-
+    const chargeInitialData = () => {
+        const saveData = localStorage.getItem('datos')
+        return saveData ? JSON.parse(saveData) : tasks
+    }
+    const [data, setData] = useState(chargeInitialData);
+    useEffect(() => {
+        localStorage.setItem('datos', JSON.stringify(data))
+    }, [data])
     const handledAdd = (task) => {
         setData([...data, task]);
     };
-
     const handleUpdateCheck = (id) => {
         const index = data.findIndex((task) => task.id === id);
         const updatedData = [...data];
         updatedData[index].isComplete = !updatedData[index].isComplete;
         setData(updatedData);
-        console.log("entro a check", data);
-
     };
-
     const handleUpdateTask = (id, name) => {
         const index = data.findIndex((task) => task.id === id);
         const updatedData = [...data];
         updatedData[index].name = name;
         setData(updatedData);
-        console.log("entro a task")
     };
-
     const handleDelete = (id) => {
         setData(data.filter((task) => task.id != id));
-        console.log("entro a Delete", data);
     };
     const completeTasks = data.filter((task) => task.isComplete === true);
     const notCompleteTasks = data.filter((task) => task.isComplete === false);
-
-
     return {
         handledAdd,
         handleUpdateCheck,
@@ -39,6 +36,7 @@ export const useTasklist = () => {
         handleDelete,
         completeTasks,
         notCompleteTasks,
+        data,
 
 
     }
